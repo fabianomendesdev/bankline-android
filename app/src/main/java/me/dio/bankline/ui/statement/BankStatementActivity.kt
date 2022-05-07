b1d7f810418cfe47ba8c0f7b42b34bfb95ce29d0
Fabiano Mendes <fabianomendesoficial@gmail.com>
2022-05-07 19:30:44 -0300

    update

---
+++ app/src/main/java/me/dio/bankline/ui/statement/BankStatementActivity.kt
@@ -0,0 +1,12 @@
+package me.dio.bankline.ui.statement
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import me.dio.bankline.R
+
+class BankStatementActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bank_statement)
+    }
+}
--- app/src/main/java/me/dio/bankline/ui/welcome/WelcomeActivity.kt
+++ app/src/main/java/me/dio/bankline/ui/welcome/WelcomeActivity.kt
@@ -1,11 +1,12 @@
-package me.dio.bankline
+package me.dio.bankline.ui.welcome
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import me.dio.bankline.R
 
-class MainActivity : AppCompatActivity() {
+class WelcomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_welcome)
     }
 }
