f4a3ec88ad6b8750b999703864036ddd162ad5e0
Fabiano Mendes <fabianomendesoficial@gmail.com>
2022-05-08 13:27:32 -0300

    update

--- app/src/main/java/me/dio/bankline/ui/statement/BankStatementActivity.kt
+++ app/src/main/java/me/dio/bankline/ui/statement/BankStatementActivity.kt
@@ -5,6 +5,11 @@ import android.os.Bundle
 import me.dio.bankline.R
 
 class BankStatementActivity : AppCompatActivity() {
+
+    companion object {
+        const val EXTRA_ACCOUNT_HOLDER = "me.dio.bankline.ui.statement.ACCOUNT_HOLDER"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bank_statement)
